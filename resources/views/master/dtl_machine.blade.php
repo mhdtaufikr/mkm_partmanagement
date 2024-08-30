fb9cf8243044ecd8cf1a92d302077f19859c33dc
Muhammad Taufik <MKM02120@ptmkm.co.id>
2024-08-30 11:12:57 +0700

    done

--- resources/views/master/dtl_machine.blade.php
+++ resources/views/master/dtl_machine.blade.php
@@ -671,7 +671,9 @@
                                                                                     <div class="modal-dialog modal-lg">
                                                                                         <div class="modal-content">
                                                                                             <div class="modal-header">
-                                                                                                <h5 class="modal-title" id="modal-detail{{ $part->id }}-label">Repair Details -  {{ $part->part->material }}</h5>
+                                                                                                <h5 class="modal-title" id="modal-detail{{ $part->id }}-label">
+                                                                                                    Repair Details - {{ $part->part ? $part->part->material : 'Part information not available' }}
+                                                                                                </h5>
                                                                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                                                             </div>
                                                                                             <div class="modal-body">
